06c3a6c032b03f64d36d33e1d9753bc27f893401
Tareq Zeadeh <tareq.ha130@yahoo.com>
2021-07-26 17:45:19 +0300

    lab 02 done

--- src/component/HornedBeast.js
+++ src/component/HornedBeast.js
@@ -1,15 +1,74 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+// import Col from 'react-bootstrap/Col'
+import Card from 'react-bootstrap/Card';
+// import Row from 'react-bootstrap/Row';
+import CardGroup from 'react-bootstrap/CardGroup';
+
+
 
 class HornedBeast extends React.Component {
+
+
+    constructor(props) {
+
+        super(props);
+        this.state = {
+            Numberofvotes: 0,
+        }
+    }
+
+    incrementNumOfPets = () => {
+        this.setState({
+            Numberofvotes: this.state.Numberofvotes + 1
+        })
+    };
+
     render() {
         return (
+
             <div>
-           
-                <h2>{this.props.title} </h2>
-               
-                <img src={this.props.url} alt={this.props.title}/>  
-                
-                <p>{this.props.description}</p>
+
+                {/* <Card>
+                    <Card.Img variant="top" src="holder.js/100px160" />
+                    <Card.Body>
+                        <Card.Title>{this.props.title}</Card.Title>
+                        <Card.Text>
+                            This is a wider card with supporting text below as a natural lead-in to
+                            additional content. This content is a little bit longer.
+                        </Card.Text>
+                    </Card.Body>
+                    <Card.Footer>
+                        <small className="text-muted">Last updated 3 mins ago</small>
+                    </Card.Footer>
+                </Card> */}
+                <CardGroup>
+                    {/* <Row xs={1} md={3} className="g-4">
+                        {Array.from({ length: 3 }).map((_, idx) => (
+                            <Col> */}
+                                <Card style={{ width: "15%", height: "25%", margin:"5%" }}>
+                                    <Card.Img onClick={this.incrementNumOfPets} variant="top" src={this.props.url} width='33px' height='250px'/>
+                                    <Card.Body>
+                                        <Card.Title>{this.props.title}</Card.Title>
+                                        <Card.Text>
+                                            Number of votes ❤️: {this.state.Numberofvotes}
+                                        </Card.Text>
+                                        <Card.Footer>
+                                            {this.props.description}
+                                        </Card.Footer>
+                                    </Card.Body>
+                                </Card>
+                             {/* </Col> */}
+                        {/* ))} */}
+                    {/* </Row> */}
+                </CardGroup> 
+
+
+                {/* <h2>{this.props.title} </h2>
+
+                <img src={this.props.url} alt={this.props.title} />
+
+                <p>{this.props.description}</p> */}
 
             </div>
         )
